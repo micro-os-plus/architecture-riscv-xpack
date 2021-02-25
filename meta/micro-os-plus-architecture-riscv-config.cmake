598387cfc0498271a3dd62b167eddaa8a438637a
Liviu Ionescu <ilg@livius.net>
2021-02-25 16:42:30 +0200

    cmake: simplify, keep only config.cmake

--- meta/micro-os-plus-architecture-riscv-config.cmake
+++ meta/micro-os-plus-architecture-riscv-config.cmake
@@ -12,11 +12,56 @@
 # https://cmake.org/cmake/help/v3.18/
 # https://cmake.org/cmake/help/v3.18/manual/cmake-packages.7.html#package-configuration-file
 
-# message(STATUS "Including micro-os-plus-architecture-riscv-config...")
+if(micro-os-plus-architecture-riscv-included)
+  return()
+endif()
+
+set(micro-os-plus-architecture-riscv-included TRUE)
+
+message(STATUS "Including micro-os-plus-architecture-riscv...")
+
+# -----------------------------------------------------------------------------
+# Local dependencies.
+
+include("${CMAKE_CURRENT_LIST_DIR}/../device/meta/xpack-helper.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/../platform/meta/xpack-helper.cmake")
 
 # -----------------------------------------------------------------------------
 
-include("${CMAKE_CURRENT_LIST_DIR}/xpack-helper.cmake")
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
 # -----------------------------------------------------------------------------
 
+if(NOT TARGET micro-os-plus-architecture-riscv-interface)
+
+  add_library(micro-os-plus-architecture-riscv-interface INTERFACE EXCLUDE_FROM_ALL)
+
+  # ---------------------------------------------------------------------------
+  # Target settings.
+
+  target_sources(
+    micro-os-plus-architecture-riscv-interface
+
+    INTERFACE
+      ${xpack_current_folder}/src/reset-entry.S
+      ${xpack_current_folder}/src/trap-entry.S
+  )
+
+  target_include_directories(
+    micro-os-plus-architecture-riscv-interface
+
+    INTERFACE
+      ${xpack_current_folder}/include
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(micro-os-plus::architecture-riscv ALIAS micro-os-plus-architecture-riscv-interface)
+  message(STATUS "micro-os-plus::architecture-riscv")
+  add_library(micro-os-plus::architecture ALIAS micro-os-plus-architecture-riscv-interface)
+  message(STATUS "micro-os-plus::architecture")
+
+endif()
+
+# -----------------------------------------------------------------------------
