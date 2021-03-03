e45bc08e715259046f98b9046ba0112d54dde4a8
Liviu Ionescu <ilg@livius.net>
2021-03-04 00:30:57 +0200

    cmake: add xpack_process_package_version()

--- meta/micro-os-plus-architecture-riscv-config.cmake
+++ meta/micro-os-plus-architecture-riscv-config.cmake
@@ -18,7 +18,7 @@ endif()
 
 set(micro-os-plus-architecture-riscv-included TRUE)
 
-message(STATUS "Including micro-os-plus-architecture-riscv...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # Local dependencies.
