507bcb70ae00da121b4975023ec76eafe0e78158
Liviu Ionescu <ilg@livius.net>
2021-03-04 10:05:34 +0200

    cmake: show xPack name@version

--- device/meta/config.cmake
+++ device/meta/config.cmake
@@ -15,7 +15,7 @@ endif()
 
 set(micro-os-plus-architecture-riscv-device-included TRUE)
 
-message(STATUS "Including micro-os-plus-architecture-riscv/device...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # The current folder.
