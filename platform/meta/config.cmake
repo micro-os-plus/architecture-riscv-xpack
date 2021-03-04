71779a50082dde902df677d6b6ecc66d5bd58b1a
Liviu Ionescu <ilg@livius.net>
2021-03-04 10:06:29 +0200

    cmake: show xPack name@version

--- platform/meta/config.cmake
+++ platform/meta/config.cmake
@@ -15,7 +15,7 @@ endif()
 
 set(micro-os-plus-architecture-riscv-platform-included TRUE)
 
-message(STATUS "Including micro-os-plus-architecture-riscv/platform...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # The current folder.
